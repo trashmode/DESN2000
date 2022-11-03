0aec05a561910942b3af98b1bdc10859fdc4adb7
trashmode <friendmichaelchai@hotmail.com>
2022-11-03 15:14:11 +1100

    got test code running for sending sensor data and found and setup ubidots decoder

--- 221101-231314-wiscore_rak4631/src/main.cpp
+++ 221101-231314-wiscore_rak4631/src/main.cpp
@@ -1,9 +1,8 @@
 /**
  * @file main.cpp
  * @author Kalina Knight
- * @brief A simple example of using the Port Schema library.
- * This example collects fake sensor data and does not actually use LoRa at all, but prints what the payload would
- * contain.
+ * @brief An example of using the Port Schema library with LoRaWAN.
+ * This example collects and sends fake sensor data.
  *
  * @version 0.1
  * @date 2021-08-24
@@ -14,8 +13,10 @@
 #include <Arduino.h>
 #include <LoRaWan-RAK4630.h> // Click to get library: https://platformio.org/lib/show/6601/SX126x-Arduino
 
-#include "Logging.h"    /**< Go here to change the logging level for the entire application. */
-#include "PortSchema.h" /**< Go here to see existing and define new sensor/port schemas. */
+#include "LoRaWAN_functs.h" /**< Go here to provide the OTAA keys & change the LoRaWAN settings. */
+#include "Logging.h"        /**< Go here to change the logging level for the entire application. */
+#include "OTAA_keys.h"      /**< Go here to set the OTAA keys (See LoRaWAN_functs README). */
+#include "PortSchema.h"     /**< Go here to see existing and define new sensor/port schemas. */
 
 // PORT/SENSOR SELECTION
 // The chosen port determines the sensor data included in the payload - see PortSchema.h
@@ -30,14 +31,15 @@ uint8_t p;
 // fill with fake data, making sure to set the validity flag to true
 sensorData sensor_data = { 1, true, 2, true, 3, true, 4, true, 5, true, 6, 7, true };
 
-// Sensor reading interval in [ms] = 2 seconds.
-const int encoding_interval = 2000;
+// Sensor reading interval in [ms] = 30 seconds.
+const int encoding_interval = 30000;
 
 // PAYLOAD ENCODING
-#define PAYLOAD_BUFFER_SIZE 64
 uint8_t payload_buffer[PAYLOAD_BUFFER_SIZE] = {};                /**< Buffer that payload data is placed in. */
 lmh_app_data_t lorawan_payload = { payload_buffer, 0, 0, 0, 0 }; /**< Struct that passes the payload buffer and relevant
                                                                     params for a LoRaWAN frame. */
+// forward declaration
+void fillPayload(void);
 
 /**
  * @brief Setup code runs once on reset/startup.
@@ -46,14 +48,17 @@ void setup() {
     // initialise the logging module - function does nothing if APP_LOG_LEVEL in Logging.h = NONE
     initLogging();
     log(LOG_LEVEL::INFO,
-        "\n====================================="
-        "\nWelcome to Simple Port Schema Example"
-        "\n=====================================");
+        "\n======================================"
+        "\nWelcome to Port Schema LoRaWAN Example"
+        "\n======================================");
 
-    // log sensor data once
-    log(LOG_LEVEL::INFO, "Sensor Data: {b: %.2f mV | t: %.2f C | h: %.2f %% | p: %lu Pa | g: %lu | l: %.5f, %.5f}",
-        sensor_data.battery_mv.value, sensor_data.temperature.value, sensor_data.humidity.value, sensor_data.pressure.value,
-        sensor_data.gas_resist.value, sensor_data.location.latitude, sensor_data.location.longitude);
+    // Init LoRaWAN
+    if (!initLoRaWAN(OTAA_KEY_APP_EUI, OTAA_KEY_DEV_EUI, OTAA_KEY_APP_KEY)) {
+        return;
+    }
+
+    // Attempt to join the network
+    startLoRaWANJoinProcedure();
 
     p = 0;
 }
@@ -62,24 +67,49 @@ void setup() {
  * @brief Loop code runs repeated after setup().
  */
 void loop() {
+    // every encoding_interval ms check if connected and then send sensor payload
     delay(encoding_interval);
-    if (p < PORT_LIST_LENGTH) {
-        // cycle over port list
-        portSchema payload_port = port_list[p++];
-
-        // reset the payload
-        memset(payload_buffer, 0, sizeof(payload_buffer));
-        lorawan_payload.buffsize = 0;
-        lorawan_payload.port = payload_port.port_number;
-
-        // encode the sensor data to lorawan_payload
-        lorawan_payload.buffsize = payload_port.encodeSensorDataToPayload(&sensor_data, payload_buffer);
-
-        // log the encoded bytes
-        char encoded_payload_bytes[3 * PAYLOAD_BUFFER_SIZE] = {};
-        for (int b = 0; b < lorawan_payload.buffsize; b++) {
-            snprintf(encoded_payload_bytes, sizeof(encoded_payload_bytes), "%s%02X ", encoded_payload_bytes, payload_buffer[b]);
+    if (isLoRaWANConnected()) {
+        log(LOG_LEVEL::DEBUG, "Send payload");
+        if (p == 0) {
+            // log sensor data
+            log(LOG_LEVEL::INFO,
+                "Sensor Data: {b: %.2f mV | t: %.2f C | h: %.2f %% | p: %lu Pa | g: %lu | l: %.5f, %.5f}",
+                sensor_data.battery_mv.value, sensor_data.temperature.value, sensor_data.humidity.value,
+                sensor_data.pressure.value, sensor_data.gas_resist.value, sensor_data.location.latitude,
+                sensor_data.location.longitude);
+        } else if (p >= PORT_LIST_LENGTH) {
+            p = 0;
         }
-        log(LOG_LEVEL::INFO, "Port: %2.d | Payload: %s", lorawan_payload.port, encoded_payload_bytes);
+        // fill lora data buffer
+        fillPayload();
+        // send data
+        sendLoRaWANFrame(&lorawan_payload);
+    } else {
+        log(LOG_LEVEL::DEBUG, "LoRaWAN not connected. Try again later.");
     }
+}
+
+/**
+ * @brief Gets the sensor data, then fills payload_buffer with the encoded data ready for sending via LoRaWAN.
+ * Follows the portSchema specified in PortSchema.h.
+ */
+void fillPayload(void) {
+    // cycle over port list
+    portSchema payload_port = port_list[p++];
+
+    // reset the payload
+    memset(payload_buffer, 0, sizeof(payload_buffer));
+    lorawan_payload.buffsize = 0;
+    lorawan_payload.port = payload_port.port_number;
+
+    // encode the sensor data to lorawan_payload
+    lorawan_payload.buffsize = payload_port.encodeSensorDataToPayload(&sensor_data, payload_buffer);
+
+    // log the encoded bytes
+    char encoded_payload_bytes[3 * PAYLOAD_BUFFER_SIZE] = {};
+    for (int b = 0; b < lorawan_payload.buffsize; b++) {
+        snprintf(encoded_payload_bytes, sizeof(encoded_payload_bytes), "%s%02X ", encoded_payload_bytes, payload_buffer[b]);
+    }
+    log(LOG_LEVEL::INFO, "Port: %2.d | Payload: %s", lorawan_payload.port, encoded_payload_bytes);
 }
